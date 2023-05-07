2d9213f3ba911902b6508fc2cd900d6a17a9f8c6
meiqi-gu <77755155+meiqi-gu@users.noreply.github.com>
2023-05-07 01:17:45 -0400

    create default API endpoint

--- server.js
+++ server.js
@@ -1,2 +1,20 @@
 import minimist from "minimist";
 import express from "express"; 
+import fs from "fs"; 
+
+var app = express(); 
+
+const args = minimist(process.argv.slice(2)); 
+const port = args.port || args.p || process.env.PORT || 5000; 
+
+// Default API endpoint
+app.use(function(req, res) {
+    const statusCode = 404; 
+    const statusMsg = 'NOT FOUND'; 
+    res.status(statusCode).end(statusCode+' '+statusMsg); 
+}); 
+
+const server = app.listen(port, () => {
+    console.log("Working.")
+}); 
+
